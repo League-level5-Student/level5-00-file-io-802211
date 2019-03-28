577412a321bff89e773403b9c019d7cf8e32166c
League <league@WTS8.attlocal.net>
2019-03-27 18:29:06 -0700

    FileRecorder, FileDecryptor, FileEncryptor(IP)
    FileRecorder, FileDecryptor, FileEncryptor(IP) 3/27/19

--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -1,6 +1,39 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileDecryptor {
 	// Create a program that opens the file created by FileEncryptor and display
 	// the decrypted message to the user in a JOptionPane.
+	
+	public static void main(String[] args) {
+		
+		String file = "";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("src/_02_File_Encrypt_Decrypt/FileEnvryptDecrypt.txt"));
+			file = br.readLine();
+			br.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String decrypted = decryptor(file);
+		JOptionPane.showMessageDialog(null, decrypted);
+	}
+	
+	static String decryptor(String s) {
+		
+		////Start HERE NEXT time\\\\
+		return s;
+	}
+	
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,7 +1,57 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	// Create a program that takes a message from the user.
 	// Use the methods in the String and Character classes to save
 	// an encrypted form of the message to a file
+	 
+	public static void main(String[] args) {
+		String message = JOptionPane.showInputDialog("Write a message :)");
+		String e = encryptor(message);
+		try {
+			FileWriter fw = new FileWriter("src/_02_File_Encrypt_Decrypt/FileEncryptDecrypt.txt");
+			fw.write(e);
+			fw.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+	
+	static String encryptor(String s) {
+	
+	String str = "";
+		
+	if(s.length()%2 != 0) {
+		for(int i = 0; i<s.length(); i++) {
+			if(i%2==0) {
+				String letter = s.substring(i, i+1);
+				str = str + letter.toUpperCase();
+			}
+			else {
+				String letter = s.substring(i, i+1);
+				str = str + letter.toLowerCase();
+			}
+		}
+	}
+	else {
+		for(int j = 0; j<s.length(); j++) {
+			if(j%2==0) {
+				String letter = s.substring(j, j+1);
+				str = str + letter.toLowerCase();
+			}
+			else {
+				String letter = s.substring(j, j+1);
+				str = str + letter.toUpperCase();
+			}
+		}
+	}
+		return str;
+	}
+	
 }
